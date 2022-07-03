fcfe61adf79f7f16b2eec1954f4354c6c9abfe17
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-03 01:41:19 -0300

    fix: README

--- services/index.tsx
+++ services/index.tsx
@@ -4,14 +4,12 @@ async function getProducts(page: number): Promise<IProductData> {
   const url = `https://wine-back-test.herokuapp.com/products?page=${page}&limit=9`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log('allProductos', data);
   return data;
 }
 async function getAllProducts(): Promise<IProductData> {
   const url = `https://wine-back-test.herokuapp.com/products?`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
@@ -19,7 +17,6 @@ async function getProductsPerPrice(page: number, price: string): Promise<IProduc
   const url = `https://wine-back-test.herokuapp.com/products?page=${page}&limit=9&filter=${price}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
@@ -27,7 +24,6 @@ async function getProductsByName(page: number, name: string): Promise<IProductDa
   const url = `https://wine-back-test.herokuapp.com/products?page=${page}&limit=9&name=${name}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
